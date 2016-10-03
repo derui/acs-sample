0db6ad07427795301bf6621abfcfb0124f98ed52
derui <derutakayu@gmail.com>
2016-10-03 23:19:55 +0900

    Fix command to launch registry in swarm

--- acs-deploy.sh
+++ acs-deploy.sh
@@ -4,6 +4,6 @@ ssh -L 2375:localhost:2375 -f -o "PasswordAuthintication no" -o "StrictHostKeyCh
 
 export DOCKER_HOST=":2375"
 
-docker-compose up -d swarm-registry
+docker run -d -p 5000:5000 --name swarmregistry -e "REGISTRY_STORAGE_AZURE_ACCOUNTKEY=$AZURE_ACCOUNT_KEY" "localhost:50000/swarm-registry"
 docker rm -f api
 docker run --name api -d -p 80:80  "localhost:5000/flask:$CIRCLE_SHA1"
