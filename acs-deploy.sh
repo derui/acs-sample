dcd91a0a911c9d7d180f1e2240b2bb7bfa00bf14
derui <derutakayu@gmail.com>
2016-10-04 23:57:45 +0900

    Fix port of swarm registry

--- acs-deploy.sh
+++ acs-deploy.sh
@@ -4,6 +4,6 @@ ssh -L 2375:localhost:2375 -f -o "StrictHostKeyChecking no" -N derui@samplemgmt.
 
 export DOCKER_HOST=":2375"
 
-docker run -d -p 5000:5000 --name swarmregistry -e "REGISTRY_STORAGE_AZURE_ACCOUNTKEY=$AZURE_ACCOUNT_KEY" "localhost:50000/swarm-registry"
+docker run -d -p 5000:5000 --name swarmregistry -e "REGISTRY_STORAGE_AZURE_ACCOUNTKEY=$AZURE_ACCOUNT_KEY" "localhost:5000/swarm-registry"
 docker rm -f api
 docker run --name api -d -p 80:80  "localhost:5000/flask:$CIRCLE_SHA1"
