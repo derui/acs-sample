1a376b3ba392ab720cd0b9d147c8b3aa0828ed35
derui <derutakayu@gmail.com>
2016-10-06 07:59:58 +0900

    Push image before into swarm

--- acs-deploy.sh
+++ acs-deploy.sh
@@ -5,7 +5,7 @@ ssh -L 2375:localhost:2375 -f -o "StrictHostKeyChecking no" -N derui@samplemgmt.
 export DOCKER_HOST=":2375"
 
 if [ "`docker ps -q | grep swarmregistry | wc -l`" != "1" ]; then
-   docker run -d -p 5000:5000 --name swarmregistry -e "REGISTRY_STORAGE_AZURE_ACCOUNTKEY=$AZURE_ACCOUNT_KEY" swarm-registry
+   docker run -d -p 5000:5000 --name swarmregistry -e "REGISTRY_STORAGE_AZURE_ACCOUNTKEY=$AZURE_ACCOUNT_KEY" "localhost:5000/swarm-registry"
 fi
 docker rm -f api
 docker run --name api -d -p 80:80  "localhost:5000/flask:$CIRCLE_SHA1"
